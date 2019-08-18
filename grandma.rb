425570bab58b8b8a970b081d25de3039ee0acbfa
Ashley Norton <nortonashleym@gmail.com>
2019-08-18 13:28:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,5 +2,8 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
 end
